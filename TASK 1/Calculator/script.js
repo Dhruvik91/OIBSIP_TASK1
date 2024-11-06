1c19f31b9c18270ab732aed86276836b4f8fe99e
Dhruvik91 <yashgondaliya04@gmail.com>
2024-11-06 09:42:47 +0530

    c

--- TASK 1/Calculator/script.js
+++ TASK 1/Calculator/script.js
@@ -1,29 +1,29 @@
-window.onload = function() {
-    var input = document.getElementById("inputBox");
-    var container = document.getElementById("container");
+window.onload = function () {
+  var input = document.getElementById("inputBox");
+  var container = document.getElementById("container");
 
-    container.addEventListener("click", function(e) {
-        buttonClick(e.target.id);
-    });
+  container.addEventListener("click", function (e) {
+    buttonClick(e.target.id);
+  });
 
-    var calc = document.getElementById("Button=");
-    calc.addEventListener("click",calculate);
+  var calc = document.getElementById("Button=");
+  calc.addEventListener("click", calculate);
 
-    var C = document.getElementById("ButtonC");
-    C.addEventListener("click", erase);
+  var C = document.getElementById("ButtonC");
+  C.addEventListener("click", erase);
 
-    function buttonClick(buttonId) {
-        if((buttonId != "ButtonC") && (buttonId != "Button=")) {
-            var button = document.getElementById(buttonId);
-             var s = buttonId;
-             s = s.replace("Button", "");
-             entries(s);
-        }
+  function buttonClick(buttonId) {
+    if (buttonId != "ButtonC" && buttonId != "Button=") {
+      var button = document.getElementById(buttonId);
+      var s = buttonId;
+      s = s.replace("Button", "");
+      entries(s);
     }
-
-    function entries(s) {
-        input.value += s;
-        /* button1: s = "1"
+  }
+  console.log();
+  function entries(s) {
+    input.value += s;
+    /* button1: s = "1"
         input.value = undefined
         entries("1")
         input.value =+ s
@@ -33,16 +33,16 @@ window.onload = function() {
         entries("2")
         input.value = input.value + s = "1" + "2" ="12"
         */
-    }
+  }
 
-    function calculate() {
-        if(input.value == ".") {
-            alert("Please Enter a Mathematical Expression");
-        }
-        input.value = eval(input.value);
+  function calculate() {
+    if (input.value == ".") {
+      alert("Please Enter a Mathematical Expression");
     }
+    input.value = eval(input.value);
+  }
 
-    function erase() {
-        input.value = "";
-    }
+  function erase() {
+    input.value = "";
+  }
 };
